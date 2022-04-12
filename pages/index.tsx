2bfafab16725abbccae0bff1c9765c3b43da9f40
João Henrique <joaoenrique.san@gmail.com>
2022-04-12 12:04:51 -0300

    feat: create an animated scroll back button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,40 @@
 import { CarGrid } from '@components';
 import type { GetStaticProps, NextPage } from 'next';
+import { useEffect, useState } from 'react';
 import { Car } from 'shared/types';
-import { MainPageContainer } from './styles';
+import { MainPageContainer, ScrollBackButton } from './styles';
 
 const Home: NextPage<{ cars: Car[] }> = (props: { cars: Car[] }) => {
+  const [showScrollBackButton, setShowScrollBackButton] = useState(false);
+
+  useEffect(() => {
+    const changeScrollPercentage = () => {
+      const winScroll =
+        document.body.scrollTop || document.documentElement.scrollTop;
+
+      const height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+      const scrolled = winScroll / height;
+
+      if (scrolled > 0.1) {
+        setShowScrollBackButton(true);
+      } else {
+        setShowScrollBackButton(false);
+      }
+    };
+    window.addEventListener('scroll', changeScrollPercentage);
+
+    return () => {
+      window.removeEventListener('scroll', changeScrollPercentage);
+    };
+  });
+
   return (
     <MainPageContainer>
       <CarGrid cars={props.cars} />
+      <ScrollBackButton size={70} showScrollButton={showScrollBackButton} />
     </MainPageContainer>
   );
 };
--- pages/styles.ts
+++ pages/styles.ts
@@ -1,3 +1,4 @@
+import { MdKeyboardArrowUp } from 'react-icons/md';
 import styled from 'styled-components';
 
 export const MainPageContainer = styled.div`
@@ -8,5 +9,25 @@ export const MainPageContainer = styled.div`
 
   @media (max-width: 500px) {
     padding: 1rem 3rem;
+  }
+`;
+
+export const ScrollBackButton = styled(MdKeyboardArrowUp)<{
+  showScrollButton: boolean;
+}>`
+  path {
+    color: white;
+  }
+  position: fixed;
+  right: 5%;
+  bottom: 10%;
+  background-color: #e6d3f1;
+  border-radius: 50%;
+  transition: 0.2s all ease-in-out;
+  transform: ${(props) => (props.showScrollButton ? 'scale(1)' : 'scale(0.0)')};
+  opacity: ${(props) => (props.showScrollButton ? '100%' : '0%')};
+  &:hover {
+    cursor: pointer;
+    background-color: #e1d1e2;
   }
 `;
